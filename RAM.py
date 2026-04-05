53afb65791e5af4dedbf5ee1580e06b4e13eed0a
victorsnr <irsamuelrodriguesv@gmail.com>
2026-04-05 09:52:33 -0300

    Modelo de memória RAM (1MB)

---
+++ RAM.py
@@ -0,0 +1,42 @@
+from array import array
+
+class RAM:
+    def __init__(self):
+        self.memory = array('L', [0]) * (1024 * 1024 // 4)
+
+    def read_word(self, address):
+        word_address = (address & 0b11111111111111111111) >> 2
+        return self.memory[word_address]
+
+    def write_word(self, address, value):
+        word_address = address & 0b111111111111111111
+        value = value & 0xFFFFFFFF
+        self.memory[word_address] = value
+
+    def read_byte(self, address):
+        word_address = (address & 0b111111111111111111) >> 2
+        word = self.memory[word_address]
+
+        byte_address = address & 0b11
+
+        byte = word >> (byte_address << 3)
+
+        return byte
+
+    def write_byte(self, address, value):
+        word_address = (address & 0b1111111111111111111) >> 2
+        byte_address = address & 0b11
+
+        mask = ~(0xFF << (byte_address << 3))
+
+        word = self.memory[word_address] & mask
+
+        value = (value & 0xFF) << (byte_address << 3)
+        word = word | value
+        self.memory[word_address] = word
+
+
+
+
+
+
