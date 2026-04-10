bfdbc3d9c8b448d4e8a503f821d0dfcab03afbbc
victorsnr <irsamuelrodriguesv@gmail.com>
2026-04-09 23:00:21 -0300

    Criação de microinstruções de teste (Contador)

--- MicroProgram.py
+++ MicroProgram.py
@@ -31,6 +31,6 @@ class MicroInst:
 """
 
 microprogram = array("L", [0]) * 512
-microprogram[0] = 0b000000001100000100101000000001000000
-microprogram[1] = 0b000000010100001110100000000101000000
-microprogram[2] = 0b000000011100000100100000000001000000
+microprogram[0] = 0b000000001000001100011000000000000000 # ADDR & JAM: Passa para a próxima instução; Processo: inicializa o contador (inicia com 1)
+microprogram[1] = 0b000000001001001110011000000000000000 # ADDR & JAM: Dá um "jump" se der overflow (resultado igual a 0), encerrando o processo; Processo: incrementa o contador em 1
+
