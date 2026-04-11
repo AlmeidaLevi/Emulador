ae51412ba3bf90d43c7a7e3ba6e5d91650c06c97
victorsnr <irsamuelrodriguesv@gmail.com>
2026-04-11 10:49:13 -0300

    Correção de erro na microintrução de teste

--- MicroProgram.py
+++ MicroProgram.py
@@ -4,8 +4,8 @@ from array import array
 # 000000000 000 000000000 000000000 000 0000
 
 microprogram = array("L", [0]) * 512
-microprogram[0] = 0b000000001000001100011000000000000000 # ADDR & JAM: Passa para a próxima instução; Processo: inicializa o contador (inicia com 1)
-microprogram[1] = 0b000000001001001110011000000000000000 # ADDR & JAM: Dá um "jump" se der overflow (resultado igual a 0), encerrando o processo; Processo: incrementa o contador em 1
+#microprogram[0] = 0b000000001000001100011000000000000000 # ADDR & JAM: Passa para a próxima instução; Processo: inicializa o contador (inicia com 1)
+#microprogram[1] = 0b000000001001001110011000000000000000 # ADDR & JAM: Dá um "jump" se der overflow (resultado igual a 0), encerrando o processo; Processo: incrementa o contador em 1
 
 microprogram[2] = 0b0000000110000000110000000000001000000 #Coloquei o valor da memoria no MDR
 
@@ -17,6 +17,6 @@ microprogram[4] = 0b0000001010000000101001000000001000000
 microprogram[5] = 0b0000001100000000101000010000000000000
 #Coloca o que tá em MDR no TOS
 
-microprogram[6] = 0b0000001110000011011000001000000000111 # multiplica H*TOS
+microprogram[6] = 0b0000001110000010111000001000000000111 # multiplica H*TOS
 
 microprogram[7] = 0
