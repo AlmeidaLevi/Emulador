27fad2a03c3eedbd3fc694e34d4d7d3e34814f83
Almeida.Levi <almeidalevi878@gmail.com>
2026-04-15 18:55:22 -0300

    Adicionei a MicroInstrução 0, aquela que é sempre lida quando o programa começa

--- MicroProgram.py
+++ MicroProgram.py
@@ -4,6 +4,9 @@ from array import array
 # 000000000 000 000000000 000000000 000 0000
 
 microprogram = array("L", [0]) * 512
+
+microprogram[0] = 0b0000000001000000000000000000000010000
+
 #microprogram[0] = 0b000000001000001100011000000000000000 # ADDR & JAM: Passa para a próxima instução; Processo: inicializa o contador (inicia com 1)
 #microprogram[1] = 0b000000001001001110011000000000000000 # ADDR & JAM: Dá um "jump" se der overflow (resultado igual a 0), encerrando o processo; Processo: incrementa o contador em 1
 
--- computador.py
+++ computador.py
@@ -6,8 +6,8 @@ dev = ControlUnit()
 dev.RAM.memory[0] = 0b00000000000000000000000000000100 # 4
 dev.RAM.memory[1] = 0b11111111111111111111111111111110 # -2 em complemento de 2
 
-dev.MPC = 2
 
-ticks = Clock.start([dev], False)
+dev.RAM.write_byte(0, 2)
 
-print(ticks)
+Clock.start([dev], False)
+
