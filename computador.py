1813ada24e4fd1d35c0980733e68573fd5b4c4a6
victorsnr <irsamuelrodriguesv@gmail.com>
2026-04-27 22:37:20 -0300

    Comandos testes

--- computador.py
+++ computador.py
@@ -3,8 +3,6 @@ import Clock
 
 dev = ControlUnit()
 
-dev.RAM.memory[100] = 10
-dev.RAM.memory[101] = 15
 
 #ADD
 # dev.RAM.write_byte(1, 0b000000010)
@@ -14,16 +12,18 @@ dev.RAM.memory[101] = 15
 # dev.RAM.write_byte(5, 0b000001000)
 # dev.RAM.write_byte(6, 0b100101110)
 
+
+
 #MOV
-dev.RAM.write_byte(1, 0b00100110)
-dev.RAM.write_byte(2, 0b00101000)
+dev.RAM.write_byte(1, 0b000101010)
+dev.RAM.write_byte(2, 0)
 dev.RAM.write_byte(3, 0)
-dev.RAM.write_byte(4, 25)
+dev.RAM.write_byte(4, 20)
 dev.RAM.write_byte(5, 1)
 
-
+print(dev.RAM.memory[4])
 ticks = Clock.start([dev], True)
 
-print(dev.CPU.OPC)
+print((dev.CPU.OPC))
 
 print(ticks)
